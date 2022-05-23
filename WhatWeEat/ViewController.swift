64be6a035cf2b3df77c474cf65e52140a9b9c8fd
Applecider <hyojuson1103@gmail.com>
2022-05-23 11:06:10 +0900

    feat: 프로젝트 생성

---
+++ WhatWeEat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatWeEat
+//
+//  Created by Hyoju Son on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
