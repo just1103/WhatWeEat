9b62673be83034d4c43e4323e7e601646ea1a587
yanghojoon <yanghojoon8487@gmail.com>
2022-05-31 12:35:26 +0900

    chore: 데모 웹 애플리케이션 테스트를 위한 파일 추가
    - 불필요한 파일 제거

--- WhatWeEat/Protocol/URLSessionProtocol.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol URLSessionProtocol {
-    func dataTask(with request: URLRequest,
-                  completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
-}
-
-extension URLSession: URLSessionProtocol { }
